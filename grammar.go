0e974be9dbb43de282e39e703eaa91d1e5cff917
Alemorii <alejoaadigi@gmail.com>
2026-04-28 17:40:17 -0300

    mini avance

--- grammar.go
+++ grammar.go
@@ -1,17 +1,25 @@
-<programa> -> <instruccion> // un programa puede ser una sola instruccion
 /* un programa podria ser 
 if sensor_himo == on then 
 	altavoz_comedor.mensaje = "PELIGRO"
 
 */
+<programa> -> <lista_instrucciones>
+
+<lista_instrucciones> -> <instruccion> | <instruccion> <lista_instrucciones>
 
 <instruccion> ->  
-	<when> 
-	|<every>
-	|<condicional>
+	<bloque_when> 
+	|<bloque_every>
+	|<bloque_if>
 	|<asignacion> 
 
-<WHEN> -> <when> <statement> <DO> 
+<bloque_when> -> <when> <condicion> <do> <lista_acciones> <end> 
 
-<statement> -> <sensor> <literal> 
-<when> -> when | WHEN
+<lista_acciones> -> <accion> | <accion> <lista_acciones>
+<accion> -> <asignacion> | <condicional> 
+
+<asignacion> -> <dispositivo> . <atributo>  = <valor> | <rango>
+
+<do>-> DO | do 
+<when> -> WHEN| when 
+<end> -> END | end 
