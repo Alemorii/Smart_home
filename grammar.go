681fcbbb418793183cc8bf7d57975a9d5a35d691
Alemorii <alejoaadigi@gmail.com>
2026-05-01 22:29:11 -0300

    producciones actuadores y sensores

--- grammar.go
+++ grammar.go
@@ -1,5 +1,5 @@
 /* un programa podria ser 
-if sensor_himo == on then 
+if sensor_humo == on then 
 	altavoz_comedor.mensaje = "PELIGRO"
 comentarios no contemplados en gramatica
 */
@@ -9,7 +9,8 @@ comentarios no contemplados en gramatica
 
 <lista_instrucciones> -> <instruccion> | <instruccion> <lista_instrucciones>
 
-<instruccion> ->  <bloque_when> 
+<instruccion> ->  
+	<bloque_when> 
 	|<bloque_every>
 	|<bloque_if>
 	|<asignacion> 
@@ -25,46 +26,29 @@ comentarios no contemplados en gramatica
 
 <accion> -> <asignacion> | <bloque_if> 
 
-<asignacion> -> <dispositivo> . <atributo>  = <valor> //bindear cada actuador 
-	|<dispositivo> . <atributo>  = <rango>
+ // <actuador> . <atributo>  = <valor> //bindear cada actuador 
+<asignacion> ->  
+	foco_.estado = Tk_bool | foco_.brillo = Tk_percent  | foco_.color = Tk_nombre
+	|aire_.estado = TK_bool | aire_.modo = Tk_discreto 	
+	|aire_.temp_obj = Tk_temp | aire.temp_act = Tk_temp //temp actual no deberia poder asignarse
+	|persiana_.posicion = Tk_percent
+	|cerradura_.estado = Tk_bool 
+	|reloj_.hora = Tk_time | reloj_.hora = Tk_fecha // reloj es de solo lectura asi que no deberia ser asignado
+	|altavoz_.volumen = Tk_percent | altavoz_.mute = Tk_Bool 
+	|altavoz_.mesaje = Tk_texto | altavoz_.email_notif = Tk_email 
+	|alarma_.estado = Tk_Bool | alarma.activada = Tk_Bool
+
+// este seria el bloque if, condicion de sensores
+//<sensor> <evaluacion> <valor> 
+
+// duda clave : ya que el actuador reloj y temp_act son solo lectura los tratamos como sensores?
+<condicion> -> 
+	sensor_temp <comparacion> Tk_temp | sensor_humedad <comparacion> tk_temp <logico> <condicion>
+	|sensor_humedad <comparacion> Tk_lux |sensor_humedad <comparacion> Tk_lux <logico> <condicion>
+	|sensor_movimiento <comparacion> Tk_Bool | sensor_humedad <comparacion> Tk_lux <condicion>
+	|sensor_humo <comparacion> Tk_Bool | sensor_humedad <comparacion> Tk_lux <condicion>
 
 //producciones directas 
-<valor> ->  BOOL_ESTADO
-	| BOOL_SENSOR
-  | TEMPERATURA
-  | PORCENTAJE
-  | TIEMPO
-  | ILUMINANCIA
-  | HORA
-  | FECHA
-  | EMAIL
-  | TEXTO
-  | NOMBRE
-
-<dispositivo> -> foco_ 
-	|aire_
-	|persiana_
-	|cerradura_
-	|reloj_
-	|altavoz_ 
-	|alarma_
-
-<asignacion> ->
-	|brillo 
-	|color 
-	|modo 
-	|temp_obj
-	|temp_act
-	|posicion 
-	|hora 
-	|fecha 
-	|volumen 
-	|mute 
-	|mensaje 
-	|email_notif 
-	|activada
-
-
 <do>-> DO | do 
 <when> -> WHEN| when 
 <end> -> END | end 
@@ -72,4 +56,8 @@ comentarios no contemplados en gramatica
 <then> -> THEN | then 
 <else> -> ELSE | else 
 <every> -> EVERY| every
+
+<comparacion> -> == |  != | < | > | >= | <= 
+<condicion> -> AND | OR | NOT 
+
 
